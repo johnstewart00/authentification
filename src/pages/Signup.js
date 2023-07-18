673b363724cc6af15a2c99875b80b60d5dbfc80a
johnstewart00 <stew.john560@gmail.com>
2023-07-17 19:03:31 -0600

    uhhh

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import './Signup.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
